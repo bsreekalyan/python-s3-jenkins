68cb92d4e71f109dbcb7937107e19e27feb88aa3
sreekalyan <sreekalyanprofessional@gmail.com>
2022-07-17 11:28:01 +0530

    Updated the Jenkins and shell configuration

--- rc_copy.py
+++ rc_copy.py
@@ -3,9 +3,10 @@ import boto3
 import bucket_check
 import os
 
+projects_list=[]
 projects=os.environ["env"]
 projects_list=projects.split(",")
-print(projects)
 
-for project in projects:
-    print(bucket_check.bucket_exists(project))
+for project in projects_list:
+    print(project)
+
