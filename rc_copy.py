c7134ca017c355c677731323467b8aedb451ac2b
sreekalyan <sreekalyanprofessional@gmail.com>
2022-07-17 11:23:36 +0530

    Updated the Jenkins and shell configuration

--- rc_copy.py
+++ rc_copy.py
@@ -7,13 +7,5 @@ projects=[]
 projects=os.environ["env"]
 print(projects)
 
-s3 = boto3.resource('s3')
- 
-#check if the bucket exists or not
-
-print(bucket_check.bucket_exists('bucket123123_1'))
-print(bucket_check.bucket_exists('sreekalyan-enterprise-1'))
-
-# Print out bucket names
-for bucket in s3.buckets.all():
-    print(bucket.name)
+for project in projects:
+    print(bucket_check.bucket_exists(project))
