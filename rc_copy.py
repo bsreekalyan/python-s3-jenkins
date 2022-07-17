3aa49dab52acaee6adc7c11a658e62d10d1dac89
sreekalyan <sreekalyanprofessional@gmail.com>
2022-07-17 12:42:05 +0530

    Updated the Jenkins and shell configuration

--- rc_copy.py
+++ rc_copy.py
@@ -1,11 +1,13 @@
 # Importing boto3 library
 import boto3
-import bucket_check
+import project_check
 import os
+
+file_key = ''
 
 projects_list=[]
 projects=os.environ["env"]
 projects_list=projects.split(",")
 
 for project in projects_list:
-    print(bucket_check.bucket_exists(project))
+    print(project_check.object_exists(project))
