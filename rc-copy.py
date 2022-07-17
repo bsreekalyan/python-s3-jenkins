46c4d06ed1ee2bcfd94265862df94ffbbd69fc0a
sreekalyan <sreekalyanprofessional@gmail.com>
2022-07-17 10:52:00 +0530

    Updated the Jenkins and shell configuration

--- rc-copy.py
+++ rc-copy.py
@@ -1,13 +1,13 @@
 # Importing boto3 library
 import boto3
-import bucket-check
+import bucketcheck
 
 s3 = boto3.resource('s3')
  
 #check if the bucket exists or not
 
-print(bucket_exists('bucket_1'))
-print(bucket_exists('sreekalyan-enterprise-'))
+print(bucketcheck.bucket_exists('bucket_1'))
+print(bucketcheck.bucket_exists('sreekalyan-enterprise-1'))
 
 # Print out bucket names
 for bucket in s3.buckets.all():
