eaf9407ffc77bd18f5f1fb31c4856df3edb371da
sreekalyan <sreekalyanprofessional@gmail.com>
2022-07-17 13:56:34 +0530

    Updated the Jenkins and shell configuration

--- project_check.py
+++ project_check.py
@@ -1,3 +1,4 @@
+from genericpath import exists
 import boto3
 from botocore.exceptions import ClientError
 
@@ -8,16 +9,13 @@ def object_exists(project_name):
    print(path)
    try:
       session = boto3.session.Session()
-      # User can pass customized access key, secret_key and token as well
       s3_resource = session.resource('s3')
-      presence=s3_resource.list_objects_v2(Bucket=bucket_name, Prefix=path)
-      print("Project exists.", bucket_name)
-      exists = True
+      s3_resource.Object(bucket_name,path).load()
+      print("The Project name is valid and accessible")
+      exists=True   
    except ClientError as error:
       error_code = int(error.response['Error']['Code'])
       if error_code == 403:
          print("Private Bucket. Forbidden Access! ", bucket_name)
       elif error_code == 404:
-         print("Bucket Does Not Exist!", bucket_name)
-      exists = False
-      return exists
+         print("Project Does Not Exist!", bucket_name)
