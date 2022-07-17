9789eb73fe69efbfa4bffad30e60e2e678b98f4d
sreekalyan <sreekalyanprofessional@gmail.com>
2022-07-17 06:35:12 +0530

    Updated the Jenkins and shell configuration

--- rc-copy.py
+++ rc-copy.py
@@ -1,13 +1,8 @@
 # Importing boto3 library
 import boto3
 
-# Creating a client connection with AWS S3
-s3 = boto3.client('s3')
+s3 = boto3.resource('s3')
  
-# Storing the client connection within rep
-response = s3.list_buckets()
- 
-# Output the bucket names
-print('Existing buckets:')
-for bucket in response['Buckets']: # For Loop to list all the buckets
-    print(f'{bucket["Name"]}')
+# Print out bucket names
+for bucket in s3.buckets.all():
+    print(bucket.name)
