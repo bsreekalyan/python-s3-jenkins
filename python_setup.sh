8772da0e875d57da37ae33157376f699b2155851
sreekalyan <sreekalyanprofessional@gmail.com>
2022-07-21 14:02:21 +0530

    commit

--- python_setup.sh
+++ python_setup.sh
@@ -3,9 +3,8 @@
 package_check () {   
    if ! which $1;
    then
-    echo "The $1 is not installed."
-    sudo apt update -y
-    sudo apt install "$1" -y
+    echo "The $1 is not installed."    
+    sudo yum install "$1" -y
     echo "The $1 is now installed."
    else
     echo "The $1 is already installed."
@@ -13,6 +12,6 @@ package_check () {
 }
 
 package_check python3
-package_check pip
+package_check pip3
 
 pip install boto3
